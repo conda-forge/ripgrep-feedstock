60f370342d6417f9afe556eacb123bb8c248cb80
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-09-19 01:06:51 +0200

    Use locked dependencies
    See https://github.com/rust-lang/cargo/issues/6983

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 # build statically linked binary with Rust
-cargo build   --features pcre2 --release
-cargo install --features pcre2 --root "$PREFIX" --path .
+cargo install --locked --features pcre2 --root "$PREFIX" --path .
 
 # strip debug symbols
 strip "$PREFIX/bin/rg"
