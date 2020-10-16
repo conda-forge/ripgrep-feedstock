8ef537945621f7986d8104fc188bae1c893c87b7
Isuru Fernando <isuruf@gmail.com>
2020-10-15 21:38:34 -0500

    remove debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-if [[ "$target_platform" == osx* ]]; then
-  ln -s $(which $CC) $BUILD_PREFIX/bin/cc
-  if [[ "$target_platform" == osx-arm64 ]]; then
-    export CFLAGS="$CFLAGS -arch arm64"
-    export CPPFLAGS="$CPPFLAGS -arch arm64"
-  fi
-fi
-
 # build statically linked binary with Rust
 cargo install --locked --features pcre2 --root "$PREFIX" --path .
 
