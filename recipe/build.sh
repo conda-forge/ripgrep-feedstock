129f35e381936c68e31f11587e423fa9db92af28
Seth Stadick <sstadick@gmail.com>
2021-09-22 09:08:17 -0600

    skip license check for linxus ppc

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,14 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cargo-bundle-licenses \
-    --format yaml \
-    --output CI.THIRDPARTY.yml \
-    --previous "${RECIPE_DIR}/THIRDPARTY.yml" \
-    --check-previous
+
+if command -v cargo-bundle-licenses &> /dev/null; then
+    cargo-bundle-licenses \
+        --format yaml \
+        --output CI.THIRDPARTY.yml \
+        --previous "${RECIPE_DIR}/THIRDPARTY.yml" \
+        --check-previous
+fi
 
 # build statically linked binary with Rust
 cargo install --locked --features pcre2 --root "$PREFIX" --path .
