7debf3a49b07190aa692e7c2623e0dd6439b2ca4
fcying <fcyingmk2@gmail.com>
2025-04-24 20:16:27 +0800

    feat: add bash zsh completion files

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,3 +15,9 @@ cargo install --no-track --locked --features pcre2 --root "$PREFIX" --path .
 # Generate + add the man page
 mkdir -p "${PREFIX}/share/man/man1"
 "$PREFIX/bin/rg" --generate man > "${PREFIX}/share/man/man1/rg.1" || true
+
+# Generate + add completion files
+mkdir -p "${PREFIX}/etc/bash_completion.d"
+"$PREFIX/bin/rg" --generate complete-bash > "${PREFIX}/etc/bash_completion.d/rg" || true
+mkdir -p "${PREFIX}/share/zsh/site-functions"
+"$PREFIX/bin/rg" --generate complete-zsh > "${PREFIX}/share/zsh/site-functions/_rg" || true
